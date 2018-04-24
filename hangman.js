e2f15e74bec45dd1c8f817c5e4c9400fda9348a1
David Rausch <David@Davids-MacBook-Pro-2.local>
2018-04-24 13:47:21 -0600

    new changes

---
+++ hangman.js
@@ -0,0 +1,17 @@
+var words = ["dothraki", "jon jnow", "nightwatch", "winterfell", "ned stark", "tyrion", 
+             "daenerys", "dragonglass", "cersei", "joffrey", "viserys", "rhaegar", "khal drogo", 
+             "winter is coming", "westeros", "direwolf", "white walkers", "dorne", "lannister",
+             "wildlings", "targaryen", "king's landing" ];
+
+var word = words[Math.random()*words.length];
+
+var answerArray = [];
+    for (var i = 0; i <word.length; i++){
+        answerArray = [i] = "_";
+    }
+
+var remainingLetters = word.length;
+
+while (remainingLetters > 0){
+    
+}
